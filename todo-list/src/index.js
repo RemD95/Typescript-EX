d38eb4e3e36aef6ac412c9d815d8993970e7f388
Remus Daniel Blaj <remoblaj@gmail.com>
2025-08-19 12:17:33 +0200

    config. ambiente

---
+++ todo-list/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHello = (name) => {
+    console.log(`Ciao, ${name}!`);
+};
+sayHello("TypeScript");
+//# sourceMappingURL=index.js.map
---
+++ todo-list/src/index.ts
@@ -0,0 +1,5 @@
+const sayHello = (name: string): void => {
+  console.log(`Ciao, ${name}!`);
+};
+
+sayHello("TypeScript");
