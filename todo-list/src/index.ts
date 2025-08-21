31e464d43053cd7a449e4aec47f2dc6531880c1d
Remus Daniel Blaj <remoblaj@gmail.com>
2025-08-21 11:06:11 +0200

    aggiungere un nuovo  todo

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,6 +1,21 @@
 import { Todo } from "./types"
 
 const todos: Todo[] = [];
+
+let currentId = 1;
+
+function addTodo(title: string):Todo{
+  const newTodo:Todo ={
+    id:currentId++,
+    title:title,
+    completed:false
+  };
+
+  todos.push(newTodo);
+  return newTodo;
+}
+const nuovoTodo = addTodo("imparare typescript :(");
+console.log("Aggiunto nuovo todo:", nuovoTodo);
 console.log("lista dei todos:",todos)
 
 const sayHello = (name: string): void => {
