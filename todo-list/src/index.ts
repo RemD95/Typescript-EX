8215fc4fdc681cf5eb949057fd226262a5c21bed
Remus Daniel Blaj <remoblaj@gmail.com>
2025-08-21 10:24:42 +0200

    implementare  una interfaccia todo

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,5 +1,11 @@
+import { Todo } from "./types"
+
+const todos: Todo[] = [];
+console.log("lista dei todos:",todos)
+
 const sayHello = (name: string): void => {
   console.log(`Ciao, ${name}!`);
 };
 
 sayHello("TypeScript");
+
